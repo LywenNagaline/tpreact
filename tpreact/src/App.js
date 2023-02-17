9f254dba736762185015454d1d82fbc5821f4539
Lywën <laurie@alarcon.eu>
2023-02-17 15:53:46 +0100

    ajout de data si localstorage vide en cours

--- tpreact/src/App.js
+++ tpreact/src/App.js
@@ -10,10 +10,21 @@ function TodoList() {
   });
   const [newTodo, setNewTodo] = useState('');
 
+  // Gère la synchro du localstorage
   useEffect(() => {
     console.log(JSON.stringify(todos))
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
+
+  //Gère du fetch des todos initiales
+  useEffect(() => {
+    const fetchData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/1/todos')
+    const data = await response.json()
+    setTodos(data)
+    }
+    fetchData()
+  }, []);
 
   const handleNewTodoChange = (event) => {
     setNewTodo(event.target.value);
@@ -22,7 +33,7 @@ function TodoList() {
   const handleNewTodoSubmit = (event) => {
     event.preventDefault();
     if (newTodo.trim() !== '') {
-      setTodos([...todos, { id: Date.now(), text: newTodo.trim(), isCompleted:false }]);
+      setTodos([...todos, { id: Date.now(), title: newTodo.trim(), completed:false }]);
       setNewTodo('');
     }
   };
@@ -33,17 +44,17 @@ function TodoList() {
   };
 
 
-  const handleTodoEdit = (todo, newText) => {
-    const newTextEdited = [...todos];
-    const todoIndex = newTextEdited.findIndex((t) => t.id === todo.id);
-    newTextEdited[todoIndex].text = newText;
-    setTodos(newTextEdited);
+  const handleTodoEdit = (todo, newtitle) => {
+    const newtitleEdited = [...todos];
+    const todoIndex = newtitleEdited.findIndex((t) => t.id === todo.id);
+    newtitleEdited[todoIndex].title = newtitle;
+    setTodos(newtitleEdited);
   };
 
   const handleTodoComplete = todo => {
     const updatedTodos = todos.map((t) => {
       if (t.id === todo.id) {
-        return { ...t, isCompleted: !t.isCompleted };
+        return { ...t, completed: !t.completed };
       }
       return t;
     });
@@ -54,15 +65,15 @@ function TodoList() {
     <div>
       <h1>Todo List</h1>
       <form onSubmit={handleNewTodoSubmit}>
-        <input type="text" value={newTodo} onChange={handleNewTodoChange} />
+        <input type="title" value={newTodo} onChange={handleNewTodoChange} />
         <button type="submit">Add Todo</button>
       </form>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <input type="text" value={todo.text} onChange={(event) => handleTodoEdit(todo, event.target.value)} />
+            <input type="title" value={todo.title} onChange={(event) => handleTodoEdit(todo, event.target.value)} />
             
-            <input type="checkbox" checked={todo.isCompleted} onChange={(event) => handleTodoComplete(todo)}
+            <input type="checkbox" checked={todo.completed} onChange={(event) => handleTodoComplete(todo)}
         />
             <button onClick={() => handleTodoDelete(todo)}>Delete</button>
           </li>
